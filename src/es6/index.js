2810ccfec325ecb70da7e608a1d5b3bd558e8edf
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2021-05-07 08:15:41 -0300

    first commit

--- src/es6/index.js
+++ src/es6/index.js
@@ -20,5 +20,23 @@ epica con enter
 asas das as`
 console.log(lorem);
 
+//repasar destructuracion (?)
 
+let array1 = [1 , 2, 3];
+let array2 = [4,5,6,];
 
+let spreadoperator = ['todo spredeado', ...array1, ...array2];
+console.log(spreadoperator);
+
+// Objetos en ecma6
+
+var nombre = 'Rafa';
+var edad = '30';
+
+let obj = {
+    'name': nombre , 
+    'age': edad
+};
+console.log(` ${obj}`);
+let obj2 = {nombre , edad}
+console.log(obj2);
